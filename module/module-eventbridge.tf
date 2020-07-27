acb30f17beb4915bfc8631273790d99439020203
vishal <vishal.bhogate07@gmail.com>
2020-07-27 13:50:24 +1000

    Custom EB with Rules

--- module/module-eventbridge.tf
+++ module/module-eventbridge.tf
@@ -1,35 +1,46 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "ap-south-1"
 }
-
-module "sns_email" {
-  source = "git::https://github.com/vishal7489/terraform-aws-sns-email-default.git?ref=1.0.3"
-
-  email_addresses = ["vishal.bhogate@brighte.com.au"]
-  display_name    = "ecr-scan-image"
-  stack_name      = "terraform-sns-stack"
-}
-
-
 
 module "eventbridge" {
   source = "../"
 
-  org_name = "test"
+  org_name = "brighte"
 
-  eventbus_rules = ["ecrscan"]
+  eventbus_rules = ["created-notification","added-notification","submitted__notification_joint","submitted__notification_vendor"]
   eventbus_event_pattern = {
-    ecrscan = {
-      description = "ECR Image Scan"
-      source      = "aws.ecr"
-      detail-type = "ECR Image Scan"
-      target      = module.sns_email.arn
-      id          = "ecr-scan-finding-scan-topic"
+    created-notification = {
+      rulename       = "finance.application.created__notification.terms.bp.primary"
+      description    = "Send terms and conditions payment plan email to primary applicant"
+      target         = "arn:aws:sqs:ap-south-1:310124165412:ms-communication-staging.fifo"
+      id             = "ms-communication-staging.fifo"
+      file           = file("eb-rule/created-notification.yaml")
+    }
+    added-notification = {
+      rulename       = "finance.application.applicant.added__notification.terms.bp.joint"
+      description    = "Send terms and conditions payment plan email to secondary applicant"
+      target         = "arn:aws:sqs:ap-south-1:310124165412:ms-communication-staging.fifo"
+      id             = "ms-communication-staging.fifo"
+      file           = file("eb-rule/added-notification.yaml")
+    }
+    submitted__notification_joint = {
+      rulename       = "finance.application.submitted__notification.joint.applicant"
+      description    = "Joint Applicant Request Email"
+      target         = "arn:aws:sqs:ap-south-1:310124165412:ms-communication-staging.fifo"
+      id             = "ms-communication-staging.fifo"
+      file           = file("eb-rule/submitted__notification_joint.yaml")
+    }
+    submitted__notification_vendor = {
+      rulename       = "finance.application.submitted__notification.vendor.thankyou"
+      description    = "Email vendor about application received."
+      target         = "arn:aws:sqs:ap-south-1:310124165412:ms-communication-staging.fifo"
+      id             = "ms-communication-staging.fifo"
+      file           = file("eb-rule/submitted__notification_vendor.yaml")
     }
   }
 
   eventbus_rule_state = "ENABLED"
-  enable_org_access   = false          #On false, event bus will be accessible only in Account
-  required_custom_bus = false          #On true, custom bus will get created for rules
+  enable_org_access   = true           #On false, event bus will be accessible only in Account
+  required_custom_bus = true           #On true, custom bus will get created for rules
   org_id              = "o-tta0g0xag0" #if enable_org_access is true, org_id should be passed
 }
