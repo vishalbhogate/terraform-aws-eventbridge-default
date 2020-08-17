c09e05afc1dec9135b2859adfe6200022846de5f
Vishal Bhogate <vishal.bhogate@brighte.com.au>
2020-08-17 14:52:21 +1000

    Adding extra parameter

--- _outputs.tf
+++ _outputs.tf
@@ -1,4 +1,3 @@
 output "eventbus_arn" {
-  count = var.custom_eb_exist ? 0 : 1
-  value = try(aws_cloudformation_stack.eventbus[0].outputs.EventBusArn, aws_cloudformation_stack.eventbus_policy[0].outputs.EventBusArn)
+  value = try(aws_cloudformation_stack.eventbus[0].outputs.EventBusArn, aws_cloudformation_stack.eventbus_policy[0].outputs.EventBusArn,"custom-bus-exist")
 }
