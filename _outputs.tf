ffede0aca1ebd668a206d84872c9335ef29c09f8
Vishal Bhogate <vishal.bhogate@brighte.com.au>
2020-08-17 14:48:29 +1000

    If Bus exists

--- _outputs.tf
+++ _outputs.tf
@@ -1,3 +1,4 @@
 output "eventbus_arn" {
+  count = var.custom_eb_exist ? 0 : 1
   value = try(aws_cloudformation_stack.eventbus[0].outputs.EventBusArn, aws_cloudformation_stack.eventbus_policy[0].outputs.EventBusArn)
 }
